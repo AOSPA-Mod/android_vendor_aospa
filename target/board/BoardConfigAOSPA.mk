fff25159424aa78c8b224247b90efb23e210db90
CuriousNom <aryanbiswas7585@gmail.com>
2026-03-15 11:35:27 +0530

    aospa: Include hardware/qcom-caf/common
    Change-Id: Ib299aeee17cdd47df39290950ac5d5a63aad3f68

--- target/board/BoardConfigAOSPA.mk
+++ target/board/BoardConfigAOSPA.mk
@@ -12,6 +12,9 @@ endif
 ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 # Include our Qualcomm Board configuration.
 include device/qcom/common/BoardConfigQcom.mk
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include hardware/qcom-caf/common/BoardConfigQcom.mk
+endif
 endif
 
 # Include our private configuration - optional.
